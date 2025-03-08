b86fa6ea0457e8e89cc69de7156dc80ea8f8d9c3
LaithMahdi <mahdilaith380@gmail.com>
2025-03-08 12:42:22 +0100

    Add OpenAPI documentation and API reference for product routes

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
 import { secureHeaders } from "hono/secure-headers";
+import { apiReference } from "@scalar/hono-api-reference";
+import { openAPISpecs } from "hono-openapi";
 import { timing } from "hono/timing";
 import { env } from "@/dotenv_config";
 import authRoutes from "@/controller/auth/auth_controller";
@@ -32,6 +34,41 @@ const app = new Hono()
   // Routes
   .route("/auth", authRoutes)
   .route("/product", productRoutes);
+
+app.get(
+  "/openapi",
+  openAPISpecs(app, {
+    documentation: {
+      info: {
+        title: "API Documentation",
+        version: "1.0.0",
+        description:
+          "This is a sample API documentation for the Hono framework. You can use this as a reference to create your own API documentation. Using the Hono framework, you can easily create API documentation with the help of the `hono-openapi` package.",
+        contact: {
+          name: "Team",
+          email: "Mahdilaith380@gmail.com",
+          url: "https://example.com",
+        },
+        licence: {
+          name: "MIT",
+          url: "https://opensource.org/licenses/MIT",
+        },
+      },
+      servers: [{ url: "http://localhost:3000/", description: "Local server" }],
+    },
+  })
+);
+
+app.get(
+  "/docs",
+  apiReference({
+    theme: "bluePlanet",
+    withDefaultFonts: true,
+    spec: { url: "http://localhost:3000/api/v1/openapi" },
+    baseServerURL: "http://localhost:3000/api/v1",
+    darkMode: true,
+  })
+);
 
 // Export the app TYPE
 export type AppType = typeof app;
--- src/controller/products/product_controller.ts
+++ src/controller/products/product_controller.ts
@@ -2,6 +2,7 @@ import { db } from "@/db";
 import { products } from "@/db/schema";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
+import { describeRoute } from "hono-openapi";
 import { desc, eq, sql } from "drizzle-orm";
 import {
   createProductSchema,
@@ -10,73 +11,129 @@ import {
 } from "./schema";
 
 const app = new Hono()
-  .get("/all", zValidator("query", paginationSchema), async (c) => {
-    const { page: pageStr, limit: limitStr, isActive } = c.req.valid("query");
-    const page = Number(pageStr);
-    const limit = Number(limitStr);
-
-    // Calculate offset for pagination
-    const offset = (page - 1) * limit;
-
-    // Build the query with filtering
-    const query = db
-      .select()
-      .from(products)
-      .where(
-        isActive !== undefined ? eq(products.isActive, isActive) : undefined
-      )
-      .limit(limit)
-      .offset(offset)
-      .orderBy(desc(products.id));
-
-    const allProducts = await query.execute();
-
-    // Get total count for pagination metadata
-    const totalCountQuery = await db
-      .select({ count: sql<number>`count(*)` })
-      .from(products)
-      .where(
-        isActive !== undefined ? eq(products.isActive, isActive) : undefined
-      )
-      .execute();
-
-    const totalItems = totalCountQuery[0].count;
-
-    // Calculate pagination metadata
-    const hasPreviousPage = page > 1;
-    const hasNextPage = page * limit < totalItems;
-
-    return c.json({
-      data: allProducts,
-      totalItems,
-      pageInfo: {
-        hasPreviousPage,
-        hasNextPage,
+  .get(
+    "/all",
+    describeRoute({
+      summary: "Get all products",
+      tags: ["Products"],
+      description: "Get all products with pagination and filtering",
+      responses: {
+        200: { description: "Successful response" },
+        404: { description: "Product not found" },
       },
-    });
-  })
-  .get("/:id", async (c) => {
-    const { id } = c.req.param();
+      parameters: [
+        {
+          in: "query",
+          name: "page",
+          schema: {
+            type: "string",
+            description: "Page number",
+          },
+        },
+        {
+          in: "query",
+          name: "limit",
+          schema: {
+            type: "string",
+            description: "Number of items per page",
+          },
+        },
+        {
+          in: "query",
+          name: "isActive",
+          schema: {
+            type: "string",
+            description: "Filter by active status",
+          },
+        },
+      ],
+    }),
+    zValidator("query", paginationSchema),
+    async (c) => {
+      const { page: pageStr, limit: limitStr, isActive } = c.req.valid("query");
+      const page = Number(pageStr);
+      const limit = Number(limitStr);
 
-    // Convert the ID to a number
-    const newID = Number(id);
+      // Calculate offset for pagination
+      const offset = (page - 1) * limit;
 
-    if (isNaN(newID)) {
-      return c.json({ error: "Invalid ID" }, 400);
+      // Build the query with filtering
+      const query = db
+        .select()
+        .from(products)
+        .where(
+          isActive !== undefined ? eq(products.isActive, isActive) : undefined
+        )
+        .limit(limit)
+        .offset(offset)
+        .orderBy(desc(products.id));
+
+      const allProducts = await query.execute();
+
+      // Get total count for pagination metadata
+      const totalCountQuery = await db
+        .select({ count: sql<number>`count(*)` })
+        .from(products)
+        .where(
+          isActive !== undefined ? eq(products.isActive, isActive) : undefined
+        )
+        .execute();
+
+      const totalItems = totalCountQuery[0].count;
+
+      // Calculate pagination metadata
+      const hasPreviousPage = page > 1;
+      const hasNextPage = page * limit < totalItems;
+
+      return c.json({
+        data: allProducts,
+        totalItems,
+        pageInfo: {
+          hasPreviousPage,
+          hasNextPage,
+        },
+      });
     }
+  )
+  .get(
+    "/:id",
+    describeRoute({
+      summary: "Get a product by ID",
+      tags: ["Products"],
+      description: "Get a product by its ID",
+      responses: {
+        200: { description: "Successful response" },
+        400: { description: "Invalid ID" },
+        404: { description: "Product not found" },
+      },
+    }),
+    async (c) => {
+      const { id } = c.req.param();
 
-    const product = await db
-      .select()
-      .from(products)
-      .where(eq(products.id, newID))
-      .execute();
+      // Convert the ID to a number and validate it
+      const newID = Number(id);
 
-    if (!product[0]) {
-      return c.json({ error: "Product not found" }, 404);
+      // Check if the ID is a valid number and is a positive integer
+      if (isNaN(newID)) {
+        return c.json({ error: "Invalid ID" }, 400);
+      }
+
+      // Fetch the product from the database
+      const product = await db
+        .select()
+        .from(products)
+        .where(eq(products.id, newID))
+        .execute();
+
+      // Check if the product was found
+      if (!product[0]) {
+        return c.json({ error: "Product not found" }, 404);
+      }
+
+      // Return the product
+      return c.json(product[0]);
     }
-
-    return c.json(product[0]);
-  })
+  )
   .post("/create", zValidator("json", createProductSchema), async (c) => {
     const { name, isActive, price, description } = c.req.valid("json");
 
